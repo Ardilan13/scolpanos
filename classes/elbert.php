aae46916b16b6f80ace66bdc9be9690c1820b47b
Dilan Corredor <dilancorr@gmail.com>
2023-04-30 21:24:47 -0500

    estetica

--- classes/elbert.php
+++ classes/elbert.php
@@ -13,181 +13,143 @@ class leerlingen
 	function liststudents()
 	{
 		$returnvalue = "";
-		$user_permission="";
+		$user_permission = "";
 		$sql_query = "";
-		$htmlcontrol="";
+		$htmlcontrol = "";
 
 		mysqli_report(MYSQLI_REPORT_STRICT);
 
 
-		if(session_status() == PHP_SESSION_NONE)
-		{
+		if (session_status() == PHP_SESSION_NONE) {
 			session_start();
 		}
 
-		if(isset($_SESSION["UserRights"]) && isset($_SESSION["User"]) && isset($_SESSION["BrokerCode"]))
-		{
+		if (isset($_SESSION["UserRights"]) && isset($_SESSION["User"]) && isset($_SESSION["BrokerCode"])) {
 			$user_name = $_SESSION["User"];
 			$broker_code = $_SESSION["BrokerCode"];
 			$user_permission = $_SESSION["UserRights"];
 
 			/* Replace all non letters with spaces */
-			preg_replace("/[^A-Za-z]/","",$broker_code);
+			preg_replace("/[^A-Za-z]/", "", $broker_code);
 
-			switch($user_permission)
-			{
+			switch ($user_permission) {
 				case "ADMIN":
 					$sql_query = "select studentnumber,voornamen,achternaam,geslacht,geboortedatum,klas from students";
-				break;
+					break;
 				case "BROKERADMIN":
 					$sql_query = "select studentnumber,voornamen,achternaam,geslacht,geboortedatum,klas from students";
-				break;
+					break;
 				default:
 					$returnvalue = "no user to list";
 					return $returnvalue;
-				break;
+					break;
 			}
-
-		}
-		else
-		{
+		} else {
 			$returnvalue = "no user to list";
 		}
 
 
-		try
-		{
+		try {
 			require_once("DBCreds.php");
 			$DBCreds = new DBCreds();
-			$mysqli=new mysqli($DBCreds->DBAddress, $DBCreds->DBUser, $DBCreds->DBPass, $DBCreds->DBSchema);
-			
-			if($select=$mysqli->prepare($sql_query))
-			{
-				if($select->bind_param("s",$user_name))
-				{
-					if($select->execute())
-					{
-						$this->error = false;
-						$result=1;
+			$mysqli = new mysqli($DBCreds->DBAddress, $DBCreds->DBUser, $DBCreds->DBPass, $DBCreds->DBSchema);
 
-						$select->bind_result($studentnumber,$voornamen,$achternaam,$geslacht,$geboortedatum,$klas);
+			if ($select = $mysqli->prepare($sql_query)) {
+				if ($select->bind_param("s", $user_name)) {
+					if ($select->execute()) {
+						$this->error = false;
+						$result = 1;
+
+						$select->bind_result($studentnumber, $voornamen, $achternaam, $geslacht, $geboortedatum, $klas);
 						$select->store_result();
 
-						if($select->num_rows > 0)
-						{
-							$htmlcontrol .= "<table id=\"cstable\" class=\"table table-striped table-bordered\">"; 
+						if ($select->num_rows > 0) {
+							$htmlcontrol .= "<table id=\"cstable\" class=\"table table-striped table-bordered\">";
 							$htmlcontrol .= "<thead><tr><th>Student#</th><th>Voornamen</th><th>Achternaam</th><th>Geslacht</th><th>Geboortedatum</th></th><th>klas</th><th>Details</th></tr></thead>";
 							$htmlcontrol .= "<tbody>";
 
-							while($select->fetch())
-							{
+							while ($select->fetch()) {
 								$fullusername = $firstname . " " . $lastname;
 
-								$htmlcontrol .="<td>". htmlentities($studentnumber) ."</td><td>". htmlentities($Voo) ."</td><td></td><td></td><td></td><td></td><td><a href='/leerling_details.php' class='link quaternary-color'>show more <i class='fa fa-angle-double-right quaternary-color'></i></a></td>	";
-
-								
+								$htmlcontrol .= "<td>" . htmlentities($studentnumber) . "</td><td>" . htmlentities($Voo) . "</td><td></td><td></td><td></td><td></td><td><a href='/leerling_details.php' class='link quaternary-color'>show more <i class='fa fa-angle-double-right quaternary-color'></i></a></td>	";
 							}
 
 							$htmlcontrol .= "</tbody>";
 							$htmlcontrol .= "</table>";
-						}
-						else
-						{
+						} else {
 							$htmlcontrol .= "No results to show";
 						}
-
-					}
-					else
-					{
+					} else {
 						/* error executing query */
 						$this->error = true;
 						$this->errormessage = $mysqli->error;
-						$result=0;
+						$result = 0;
 
-						if($this->debug)
-						{
+						if ($this->debug) {
 							print "error executing query" . "<br />";
 							print "error" . $mysqli->error;
 						}
 					}
-				}
-				else
-				{
+				} else {
 					/* error binding parameters */
 					$this->error = true;
 					$this->errormessage = $mysqli->error;
-					$result=0;
+					$result = 0;
 
-					if($this->debug)
-					{
+					if ($this->debug) {
 						print "error binding parameters";
 					}
 				}
-
-			}
-			else
-			{
+			} else {
 				/* error preparing query */
 				$this->error = true;
 				$this->errormessage = $mysqli->error;
-				$result=0;
+				$result = 0;
 
-				if($this->debug)
-				{
+				if ($this->debug) {
 					print "error preparing query";
 				}
 			}
 
 			$returnvalue = $htmlcontrol;
 			return $returnvalue;
-
-		}
-		catch(Exception $e)
-		{
+		} catch (Exception $e) {
 			$this->error = true;
 			$this->errormessage = $e->getMessage();
-			$result=0;
+			$result = 0;
 
-			if($this->debug)
-			{
+			if ($this->debug) {
 				print "exception: " . $e->getMessage();
 			}
-
-			
 		}
 
 		return $returnvalue;
-
-
 	}
 
 
-	function _generate_user_button($action,$username,$buttontext)
+	function _generate_user_button($action, $username, $buttontext)
 	{
 		$returnvalue = "";
 
 		$btnclass_enable = "btn btn-success btn-action";
 		$btnclass_disable = "btn btn-danger btn-action";
 		$btnclass_resetpwd = "btn btn-link resetpassword";
-		
-		switch($action)
-		{
+
+		switch ($action) {
 			case "enable":
 				$returnvalue = "<a href=\"#\" class=\"$btnclass_enable\" data-action-user=\"enable\" data-username=\"$username\">$buttontext</a>";
-			break;
+				break;
 			case "disable":
 				$returnvalue = "<a href=\"#\" class=\"$btnclass_disable\" data-action-user=\"disable\" data-username=\"$username\">$buttontext</a>";
-			break;
+				break;
 			case "reset":
 				$returnvalue = "<a href=\"#\" class=\"$btnclass_resetpwd\" data-toggle=\"modal\" data-target=\"#user\" data-username=\"$username\">$buttontext</a>";
-			break;
+				break;
 			default:
 
-			break;
+				break;
 		}
 
 		return $returnvalue;
 	}
-
-
 }
