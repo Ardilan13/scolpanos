2223e5ad7be357f783d5ee03a34b76106e23cb5d
Ardilan <dilancorr@gmail.com>
2023-06-23 10:03:06 -0500

    Creacion audit cijfers

--- ajax/update_cijfers.php
+++ ajax/update_cijfers.php
@@ -1,8 +1,12 @@
 <?php
-
+session_start();
 
 require_once("../classes/spn_cijfers.php");
 require_once("../config/app.config.php");
+require_once("../classes/spn_audit.php");
+
+$spn_audit = new spn_audit();
+$UserGUID = $_SESSION['UserGUID'];
 
 /*
 	configuration for the detail table to be shown on screen
@@ -12,46 +16,38 @@ require_once("../config/app.config.php");
 $baseurl = appconfig::GetBaseURL();
 
 
-if(session_status() == PHP_SESSION_NONE)
-{
+if (session_status() == PHP_SESSION_NONE) {
 	session_start();
 }
 
-	if(isset($_SESSION["UserRights"]) && isset($_SESSION["SchoolID"]) && isset($_POST["studentid"]) && isset($_POST["cijfername"]) && isset($_POST["cijfervalue"]) && isset($_POST["klas"]) && isset($_POST["rapport"]) && isset($_POST["vak"]))
-	{
+if (isset($_SESSION["UserRights"]) && isset($_SESSION["SchoolID"]) && isset($_POST["studentid"]) && isset($_POST["cijfername"]) && isset($_POST["cijfervalue"]) && isset($_POST["klas"]) && isset($_POST["rapport"]) && isset($_POST["vak"])) {
 
 
-		/* Add logic to check if cijfer is a numeric value and is between 0 and 10 */
-		if(is_numeric($_POST["cijfervalue"]) && $_POST["cijfervalue"] >= 0 && $_POST["cijfervalue"] <= 10)
-		{
-			//print "debug: is a numberic value between 0 and 10";
-		}
-		else
-		{
-			//print "debug: is not a numeric value or not between 0 and 10";
-		}
-
-
-
-		if($_SESSION["UserRights"] == "DOCENT" ||$_SESSION["UserRights"] == "TEACHER"  || $_SESSION["UserRights"] == "ASSISTENT")
-		{
-			if(isset($_SESSION["Class"]))
-			{
-				$s = new spn_cijfers();
-				//function savecijfer($schoolid,$studentid_in,$cijfer_number_in,$cijfer_value_in,$klas_in,$rap_in,$vak_in)
-				print $s->savecijfer($_SESSION["SchoolJaar"],$_SESSION["SchoolID"],$_POST["id_cijfer"],$_POST["studentid"],$_POST["cijfername"],$_POST["cijfervalue"],$_POST["klas"],$_POST["rapport"],$_POST["vak"]);
-			//	print $s->mysqlierror;
-			}
-		}
-		else if($_SESSION["UserRights"] == "BEHEER" || $_SESSION["UserRights"] == "ADMINISTRATIE" || $_SESSION["UserRights"] == "ONDERSTEUNING")
-		{
-			$s = new spn_cijfers();
-			//function savecijfer($schoolid,$studentid_in,$cijfer_number_in,$cijfer_value_in,$klas_in,$rap_in,$vak_in)
-			print $s->savecijfer($_SESSION["SchoolJaar"],$_SESSION["SchoolID"],$_POST["id_cijfer"],$_POST["studentid"],$_POST["cijfername"],$_POST["cijfervalue"],$_POST["klas"],$_POST["rapport"],$_POST["vak"]);
-
-			//print $s->mysqlierror;
-		}
+	/* Add logic to check if cijfer is a numeric value and is between 0 and 10 */
+	if (is_numeric($_POST["cijfervalue"]) && $_POST["cijfervalue"] >= 0 && $_POST["cijfervalue"] <= 10) {
+		//print "debug: is a numberic value between 0 and 10";
+	} else {
+		//print "debug: is not a numeric value or not between 0 and 10";
 	}
 
 
-?>
+
+	if ($_SESSION["UserRights"] == "DOCENT" || $_SESSION["UserRights"] == "TEACHER"  || $_SESSION["UserRights"] == "ASSISTENT") {
+		if (isset($_SESSION["Class"])) {
+			$spn_audit->create_audit($UserGUID, 'cijfers', 'update cijfer  id:' . $_POST["id_cijfer"] . ' cijfer:' . $_POST['cijfervalue'], appconfig::GetDummy());
+
+			$s = new spn_cijfers();
+			//function savecijfer($schoolid,$studentid_in,$cijfer_number_in,$cijfer_value_in,$klas_in,$rap_in,$vak_in)
+			print $s->savecijfer($_SESSION["SchoolJaar"], $_SESSION["SchoolID"], $_POST["id_cijfer"], $_POST["studentid"], $_POST["cijfername"], $_POST["cijfervalue"], $_POST["klas"], $_POST["rapport"], $_POST["vak"]);
+			//	print $s->mysqlierror;
+		}
+	} else if ($_SESSION["UserRights"] == "BEHEER" || $_SESSION["UserRights"] == "ADMINISTRATIE" || $_SESSION["UserRights"] == "ONDERSTEUNING") {
+		$spn_audit->create_audit($UserGUID, 'cijfers', 'update cijfer  id:' . $_POST["id_cijfer"] . ' cijfer:' . $_POST['cijfervalue'], appconfig::GetDummy());
+
+		$s = new spn_cijfers();
+		//function savecijfer($schoolid,$studentid_in,$cijfer_number_in,$cijfer_value_in,$klas_in,$rap_in,$vak_in)
+		print $s->savecijfer($_SESSION["SchoolJaar"], $_SESSION["SchoolID"], $_POST["id_cijfer"], $_POST["studentid"], $_POST["cijfername"], $_POST["cijfervalue"], $_POST["klas"], $_POST["rapport"], $_POST["vak"]);
+
+		//print $s->mysqlierror;
+	}
+}
