5bfde297415067372f3ae3f218eaef9f558689c2
Dilan Corredor <dilancorr@gmail.com>
2024-04-05 00:14:29 -0500

    api para los reports

--- api_reports.php
+++ api_reports.php
@@ -23,12 +23,14 @@ if ($result->num_rows > 0) {
         }
 
         $get_extra = "SELECT e.schooljaar,e.klas,e.rapnummer," . $vak . ",e.oc1,e.oc2,e.oc3,e.oc4,e.oc5,e.oc6,e.oc7,e.oc8,e.oc9,e.oc10,e.oc11,e.oc12,e.oc13,e.oc14,e.oc15,e.oc16,e.oc17,e.oc18,e.oc19,e.oc20
-            FROM le_cijfersextra e INNER JOIN " . $vaken . " WHERE e.schoolid = " . $row["id"] . " ORDER BY e.rapnummer";
+            ,w.c1,w.c2,w.c3,w.c4,w.c5,w.c6,w.c7,w.c8,w.c9,w.c10,w.c11,w.c12,w.c13,w.c14,w.c15,w.c16,w.c17,w.c18,w.c19,w.c20
+            FROM le_cijfersextra e INNER JOIN " . $vaken . " LEFT JOIN le_cijferswaarde w ON e.schooljaar = w.schooljaar AND e.klas = w.klas AND e.rapnummer = w.rapnummer AND e.vak = w.vak WHERE e.schoolid = " . $row["id"] . " ORDER BY e.rapnummer";
         $result_extra = mysqli_query($mysqli, $get_extra);
         while ($row_extra = mysqli_fetch_assoc($result_extra)) {
             for ($i = 1; $i <= 20; $i++) {
                 $oc = "oc" . $i;
-                if ($row_extra[$oc] > 0) {
+                $w = "c" . $i;
+                if ($row_extra[$oc] != NULL || $row_extra[$w] > 0) {
                     $d = array();
                     $d['schoolId'] = $row['api_id'];
                     $d['period'] = $row_extra['schooljaar'];
@@ -36,36 +38,12 @@ if ($result->num_rows > 0) {
                     $d['vak'] = $row_extra[substr($vak, 2)];
                     $d['id'] = $row_extra['rapnummer'];
                     $d['description'] = $row_extra[$oc];
+                    $d['percentile'] = $row_extra[$w];
+                    $d['position'] = $i;
                     $data[] = $d;
                 }
             }
         }
-
-        // if ($row["type"] == 1) {
-        //     $get_periods = "SELECT DISTINCT cp.schooljaar FROM le_cijfers_ps cp WHERE cp.school_id = " . $row["id"] . " AND cp.gemiddelde > 0.0 ORDER BY cp.schooljaar";
-        //     $result_periods = mysqli_query($mysqli, $get_periods);
-        //     while ($row_periods = mysqli_fetch_assoc($result_periods)) {
-        //         $d = array();
-        //         $d['schoolId'] = $row['api_id'];
-        //         $d['range'] = $row_periods['schooljaar'];
-        //         $d['start'] = substr($row_periods['schooljaar'], 0, 4);
-        //         $d['end'] = substr($row_periods['schooljaar'], 5, 4);
-        //         $data[] = $d;
-        //     }
-        // } else {
-        //     $get_periods = "SELECT e.schooljaar,e.klas,e.rapnummer,e.vak,e.oc1,e.oc2,e.oc3,e.oc4,e.oc5,e.oc6,e.oc7,e.oc8,e.oc9,e.oc10,e.oc11,e.oc12,e.oc13,e.oc14,e.oc15,e.oc16,e.oc17,e.oc18,e.oc19,e.oc20
-        //     FROM le_cijfersextra e
-        //     WHERE e.schoolid = " . $row["id"] . " ORDER BY e.rapnummer";
-        //     $result_periods = mysqli_query($mysqli, $get_periods);
-        //     while ($row_periods = mysqli_fetch_assoc($result_periods)) {
-        //         $d = array();
-        //         $d['schoolId'] = $row['api_id'];
-        //         $d['range'] = $row_periods['schooljaar'];
-        //         $d['start'] = substr($row_periods['schooljaar'], 0, 4);
-        //         $d['end'] = substr($row_periods['schooljaar'], 5, 4);
-        //         $data[] = $d;
-        //     }
-        // }
     }
 }
 
